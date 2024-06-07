300c3a12c411f8dd6457d89e2e3e65f967bfcb8d
Desirée Constantino <117848187+Constdesire@users.noreply.github.com>
2024-06-07 17:14:41 -0300

    Update Repository.kt

--- app/src/main/java/com/example/myapp/viewModel/Repository.kt
+++ app/src/main/java/com/example/myapp/viewModel/Repository.kt
@@ -4,13 +4,16 @@ import com.example.myapp.roomDB.Pessoa
 import com.example.myapp.roomDB.PessoaDataBase
 
 class Repository(private val db: PessoaDataBase) {
+    // inserir ou atualizar uma pessoa no banco de dados
     suspend fun upsertPessoa(pessoa: Pessoa){
         db.pessoaDao().upsertPessoa(pessoa)
     }
 
+    // excluir uma pessoa do banco de dados
     suspend fun deletePessoa(pessoa: Pessoa){
         db.pessoaDao().deletePessoa(pessoa)
     }
 
+    //  obter todas as pessoas do banco de dados
     fun getAllPessoa() = db.pessoaDao().getAllPessoa()
 }
