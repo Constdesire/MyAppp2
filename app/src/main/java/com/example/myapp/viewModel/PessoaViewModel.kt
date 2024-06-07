b6a98a7870370c5e42fa720774cc7fe923c44901
Desirée Constantino <117848187+Constdesire@users.noreply.github.com>
2024-06-07 17:13:22 -0300

    Update PessoaViewModel.kt

--- app/src/main/java/com/example/myapp/viewModel/PessoaViewModel.kt
+++ app/src/main/java/com/example/myapp/viewModel/PessoaViewModel.kt
@@ -7,14 +7,17 @@ import com.example.myapp.roomDB.Pessoa
 import kotlinx.coroutines.launch
 
 class PessoaViewModel(private val repository: Repository): ViewModel() {
+    // obter todas as pessoas do banco de dados
     fun getPessoa() = repository.getAllPessoa().asLiveData(viewModelScope.coroutineContext)
 
+    // inserir ou atualizar uma pessoa na base de dados
     fun upsertPessoa(pessoa: Pessoa){
         viewModelScope.launch {
             repository.upsertPessoa(pessoa)
         }
     }
 
+    // excluir uma pessoa da base de dados 
     fun deletePessoa(pessoa: Pessoa){
         viewModelScope.launch {
             repository.deletePessoa(pessoa)
