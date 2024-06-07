5ffe411d9b0f13dfc797c93ccd94a35a09d33c17
Desirée Constantino <117848187+Constdesire@users.noreply.github.com>
2024-06-07 17:08:01 -0300

    Update PessoaDao.kt

--- app/src/main/java/com/example/myapp/roomDB/PessoaDao.kt
+++ app/src/main/java/com/example/myapp/roomDB/PessoaDao.kt
@@ -6,15 +6,16 @@ import androidx.room.Query
 import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
 
-@Dao
+@Dao // Esta interface é marcada como um Data Access Object (DAO), que fornece métodos para acessar e manipular dados na base de dados Room
 interface PessoaDao {
+// atualiza ou insere uma entidade no banco de dados
+    @Upsert 
+    suspend fun upsertPessoa(pessoa: Pessoa) 
+//  exclui uma entidade da base de dados
+    @Delete 
+    suspend fun deletePessoa(pessoa: Pessoa) 
+// seleciona todas as entidades Pessoa da tabela
+    @Query("SELECT * FROM pessoa") 
+    fun getAllPessoa():
 
-    @Upsert
-    suspend fun upsertPessoa(pessoa: Pessoa)
-
-    @Delete
-    suspend fun deletePessoa(pessoa: Pessoa)
-
-    @Query("SELECT * FROM pessoa")
-    fun getAllPessoa(): Flow<List<Pessoa>>
 }
