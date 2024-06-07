d564be46700c335f401ca39914c06c056cce703a
Desirée Constantino <117848187+Constdesire@users.noreply.github.com>
2024-06-07 17:15:07 -0300

    Update PessoaViewModel.kt

--- app/src/main/java/com/example/myapp/viewModel/PessoaViewModel.kt
+++ app/src/main/java/com/example/myapp/viewModel/PessoaViewModel.kt
@@ -10,14 +10,14 @@ class PessoaViewModel(private val repository: Repository): ViewModel() {
     // obter todas as pessoas do banco de dados
     fun getPessoa() = repository.getAllPessoa().asLiveData(viewModelScope.coroutineContext)
 
-    // inserir ou atualizar uma pessoa na base de dados
+    // inserir ou atualizar uma pessoa no banco 
     fun upsertPessoa(pessoa: Pessoa){
         viewModelScope.launch {
             repository.upsertPessoa(pessoa)
         }
     }
 
-    // excluir uma pessoa da base de dados 
+    // excluir uma pessoa do banco
     fun deletePessoa(pessoa: Pessoa){
         viewModelScope.launch {
             repository.deletePessoa(pessoa)
